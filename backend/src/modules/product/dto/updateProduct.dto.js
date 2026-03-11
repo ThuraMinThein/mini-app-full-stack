5cdafeb34685b977f980a46c160d9d07be47c2f4
thuraminthein <thur7012@gmail.com>
2026-03-11 10:34:37 +0630

    feat: product input validation optional in update

--- backend/src/modules/product/dto/updateProduct.dto.js
+++ backend/src/modules/product/dto/updateProduct.dto.js
@@ -2,21 +2,27 @@ import { body } from "express-validator";
 
 export const updateProductValidation = [
     body("name")
+        .optional()
         .isString()
         .withMessage("Name must be a string"),
     body("description")
+        .optional()
         .isString()
         .withMessage("Description must be a string"),
     body("price")
+        .optional()
         .isFloat({ gt: 0 })
         .withMessage("Price must be a positive number"),
     body("inPrice")
+        .optional()
         .isFloat({ gt: 0 })
         .withMessage("In Price must be a positive number"),
     body("inStock")
+        .optional()
         .isInt({ gt: 0 })
         .withMessage("Stock must be a positive integer"),
     body("unit")
+        .optional()
         .isString()
         .withMessage("Unit must be a string"),
 ];
